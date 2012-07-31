876fc98333fb0bb35676e0aebc8eca4cc584f0b2
Matthew Nguyen <mnguyen916@gmail.com>
2012-07-30 20:20:39 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.7'
+gem 'json'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'quiet_assets'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'twitter-bootstrap-rails'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = '0fd7071fe04eda004b998b7a747f1e7cc137804d797e28ebfda9aeaef0f824aae328d7f95667f3b21a3677bd1dc9113a8df38c8280d7a9629dc6911dffa0292f'
